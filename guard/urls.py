80e9fa969f337da0a0a63385a2cfc07ed38eca9b
timmy-edibo <timothyedibo@gmail.com>
2022-06-04 08:55:00 +0100

    modified project url

--- guard/urls.py
+++ guard/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
     # path('store/', include('core.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
